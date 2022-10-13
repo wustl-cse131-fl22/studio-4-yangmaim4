392d943d295f30776f8f16b4341137e43dca3824
yangy <yangy@Yang.dhcp.wustl.edu>
2022-10-13 14:08:57 -0500

    finished.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,11 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(0.2, 0.2, 0.2, 0.2);
+		StdDraw.filledRectangle(0.8, 0.2, 0.2, 0.2);
+		StdDraw.filledRectangle(0.8, 0.8, 0.2, 0.2);
+		StdDraw.filledRectangle(0.2, 0.8, 0.2, 0.2);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,33 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape=in.next();
+		int red=in.nextInt();
+		int green=in.nextInt();
+		int blue=in.nextInt();
+		boolean filled=in.nextBoolean();
+		if (shape.equals("rectangle"))
+		{
+			double x1=in.nextDouble();
+			double y1=in.nextDouble();
+			double x2=in.nextDouble();
+			double y2=in.nextDouble();
+			StdDraw.clear();
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledRectangle(x1, y1, x2, y2);
+		}
+		else if (shape.equals("triangle"))
+		{
+			double[] x=new double[3];
+			double[] y=new double[3];
+			for (int i=0;i<3;i++)
+			{
+				x[i]=in.nextDouble();
+				y[i]=in.nextDouble();
+			}
+			StdDraw.clear();
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledPolygon(x, y);
+		}
 	}
 }
